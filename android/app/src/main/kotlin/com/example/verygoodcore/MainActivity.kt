b37f5d9452a3a0e85eb90006f18b63995604a558
narven <narvencito@gmail.com>
2024-01-02 17:46:10 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.narven.music
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:music/app/app.dart';
+import 'package:music/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
